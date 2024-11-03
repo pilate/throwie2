c5d67bb7f2be5e32b2298c71f710b7f6eb8d8bdd
Pilate <noptical@gmail.com>
2024-11-03 08:53:04 -0800

    shave 18 bytes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
 rm avrgcc/throwie2.*
-avr-gcc -mmcu=attiny5 -Wl,--print-memory-usage -fstack-usage -Wall -Os -o avrgcc/throwie2.elf main.c
+avr-gcc -mmcu=attiny5 -Wl,--print-memory-usage -Wl,--gc-sections -fstack-usage -Wall -Os -o avrgcc/throwie2.elf main.c
 avr-objcopy -j .text -j .data -O ihex avrgcc/throwie2.elf avrgcc/throwie2.hex
